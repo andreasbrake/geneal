760ce1c081f14cb0d7f7a89967dd1383495d3922
andreasbrake <andreas@andreasbrake.ca>
2015-06-10 03:47:44 -0400

    updated package and data structure

--- init_db.js
+++ init_db.js
@@ -1,6 +1,25 @@
 var fs = require('fs')
+var pg = require('pg');
+var conString = "postgres://postgres:admin@localhost/mydb";
 
 function init(){
+	var client = new pg.Client(conString);
+	client.connect(function(err) {
+		if(err) {
+			return console.error('could not connect to postgres', err);
+		}
+
+		client.query('SELECT NOW() AS "theTime"', function(err, result) {
+			if(err) {
+				return console.error('error running query', err);
+			}
+
+			console.log(result.rows[0].theTime);
+			client.end();
+		});
+	});
+}
+function get_files(){
 	fs.readdir("./data",function(err, files){
 		for(var i=0; i<files.length; i++){
 			var file = files[i]
@@ -8,7 +27,7 @@ function init(){
 				add_file(file)
 			}
 		}
-	})
+	})	
 }
 function add_file(filename){
 	fs.readFile('./data/' + filename, 'utf8', function (err, data) {
